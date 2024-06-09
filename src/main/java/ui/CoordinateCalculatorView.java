3da3083068779b24c94c2d71524ecf222728d045
yoonwoo-kim <ynwookim98@gmail.com>
2024-06-09 21:39:01 +0900

    Feat: 도형 검증 및 숫자 범위 검증로직 추가

--- src/main/java/ui/CoordinateCalculatorView.java
+++ src/main/java/ui/CoordinateCalculatorView.java
@@ -2,6 +2,8 @@ package ui;
 
 import coordinate.shape.Shape;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CoordinateCalculatorView {
 
@@ -9,6 +11,9 @@ public class CoordinateCalculatorView {
     private final String INPUT_MESSAGE = "좌표를 입력하세요.";
     private final String ERROR_MESSAGE = "잘못된 값을 입력했습니다.";
     private final Integer MAX_NUMBER = 24;
+    private static final String SQUARE_PATTERN = "\\((\\d+),(\\d+)\\)-\\((\\d+),(\\d+)\\)-\\((\\d+),(\\d+)\\)-\\((\\d+),(\\d+)\\)";
+    private static final String TRIANGLE_PATTERN = "\\((\\d+),(\\d+)\\)-\\((\\d+),(\\d+)\\)-\\((\\d+),(\\d+)\\)";
+    private static final String LINE_PATTERN = "\\((\\d+),(\\d+)\\)-\\((\\d+),(\\d+)\\)";
 
     public CoordinateCalculatorView() {
         scanner = new Scanner(System.in);
@@ -24,7 +29,30 @@ public class CoordinateCalculatorView {
     }
 
     public boolean validateUserInput(String userInput) {
-        // TODO: 검증로직 추가
+        // TODO: 검증로직 추가 - 사각형일 때 마름모 및 사다리꼴 확인
+        // 1. 사각형, 삼각형, 선인 입력한 허용
+        // 2. 숫자 범위 검증
+
+        Pattern squarePattern = Pattern.compile(SQUARE_PATTERN);
+        Pattern trianglePattern = Pattern.compile(TRIANGLE_PATTERN);
+        Pattern linePattern = Pattern.compile(LINE_PATTERN);
+
+        Matcher squareMatcher = squarePattern.matcher(userInput);
+        Matcher triangleMatcher = trianglePattern.matcher(userInput);
+        Matcher lineMatcher = linePattern.matcher(userInput);
+        if (!squareMatcher.matches() && !triangleMatcher.matches() && !lineMatcher.matches()) {
+            return false;
+        }
+
+        Pattern numberPattern = Pattern.compile("\\d+");
+        Matcher numberMatcher = numberPattern.matcher(userInput);
+        while (numberMatcher.find()) {
+            int number = Integer.parseInt(numberMatcher.group());
+            if (number < 0 || number > 24) {
+                return false;
+            }
+        }
+
         return true;
     }
 
@@ -42,9 +70,10 @@ public class CoordinateCalculatorView {
             }
             System.out.println("  |");
         }
-        System.out.println("  +-------------------------------------------------------------------");
+        System.out.println(
+                "  +-------------------------------------------------------------------");
         System.out.print(" ");
-        for (int i = 0; i <= MAX_NUMBER; i+=2) {
+        for (int i = 0; i <= MAX_NUMBER; i += 2) {
             System.out.print(i + "    ");
         }
         System.out.println();
